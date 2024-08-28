b0de9b6f5819ce5ce2cb0c9a81ee9c255b72b9d7
Ritik <rsingh2206@daxsws.com>
2024-08-28 10:56:58 +0530

    day 2 updated code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { TextField, InputLabel, InputAdornment, FormControl, Box, OutlinedInput, Button, Input } from '@mui/material'
-import IconButton from '@mui/material/IconButton';
+import React, { useRef, useState } from 'react';
+import { TextField,InputAdornment, IconButton} from '@mui/material'
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Link } from 'react-router-dom'
 import { Navbar } from '../components/Navbar';
@@ -11,6 +10,8 @@ export const Login = () => {
   function containsAlphabetsNumbersAndSpecialChars(str) {
     return (/[A-Za-z]/.test(str) && /[0-9]/.test(str) && /[!@#$%^&*(),.?":{}|<>]/.test(str));
   }
+
+  // const formRef = useRef({username:'',password:''});
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -23,38 +24,33 @@ export const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!containsAlphabetsNumbersAndSpecialChars(username))return setUserNameError(prev => !prev);
-    if(username && password)return alert('Your details has been verified, Thanks!');
+    if (!containsAlphabetsNumbersAndSpecialChars(username)) return setUserNameError(prev => !prev);
+    if (username && password) return alert('Your details has been verified, Thanks!');
     else return alert('Fill all the details')
   }
 
   return (
     <div>
       <Navbar headtext={'Login'} headtextSize={'4xl'} paraText={'Sign in to continue'} />
-      <Box
-        component="form"
-        sx={{ '& .MuiTextField-root': { m: 1 } }}
-        noValidate
-        autoComplete="off"
-        className='mt-[7vh] w-[70vw] mx-auto'
-        onSubmit={handleSubmit}
-      >
-        <div>
+      <form autoComplete='off' className='mt-[7vh] w-[70vw] mx-auto' onSubmit={handleSubmit}>
+        <div className='flex flex-col items-center gap-y-10'>
           <TextField id="standard-basic" error={userNameError} helperText={userNameError ? 'USERNAME must contain combination of alphanumeric values with special characters only' : ''} variant="standard" className='w-full lg:w-1/2'
             label="USERNAME" color="success" onKeyDown={(event) => {
               if (!ALPHA_NUM_CHAR_REGEX.test(event.key)) event.preventDefault()
               setUsername(event.target.value);
             }}
+            InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
+            inputProps={{sx:{paddingLeft: '15px'}}}
           />
-        </div>
-        <div>
           <TextField id="standard-basic2" error={usernameAndPasswordMatchingError} helperText={usernameAndPasswordMatchingError ? 'USERNAME and PASSWORD should not be same' : ''} variant="standard" className='w-full lg:w-1/2'
             type={showPassword ? 'text' : 'password'}
             label="PASSWORD" color="success" onKeyDown={(event) => {
               if (!ALPHA_NUM_CHAR_REGEX.test(event.key)) event.preventDefault()
               setPassword(event.target.value);
             }}
+            InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
             InputProps={{
+              sx:{paddingLeft: '15px'},
               endAdornment:
                 <InputAdornment position="end">
                   <IconButton
@@ -64,14 +60,13 @@ export const Login = () => {
                   >
                     {showPassword ? <VisibilityOff /> : <Visibility />}
                   </IconButton>
-                </InputAdornment>,
-              // classes: {adornedEnd: classes.adornedEnd}
+                </InputAdornment>
             }}
           />
         </div>
-        <button disabled={usernameAndPasswordMatchingError} className='bg-[#0a856d] px-14 py-3 rounded-xl mt-10'>LOGIN</button>
-        <p className='mt-[2vh]'>Don't have account? <Link className='text-[blue]' to='/signup'>SignUp</Link></p>
-      </Box>
+        <button disabled={usernameAndPasswordMatchingError} className='bg-[#0a856d] text-white px-14 py-2 rounded-lg mt-14'>LOGIN</button>
+        <p className='mt-[2vh]'>Don't have account? <Link className='underline' to='/signup'>SignUp</Link></p>
+      </form>
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { TextField, nputAdornment, FormControl, Box, OutlinedInput, Button, Input, IconButton, InputAdornment } from '@mui/material'
+import React, {useState} from 'react';
+import { TextField,IconButton, InputAdornment } from '@mui/material'
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Navbar } from '../components/Navbar';
 
@@ -9,6 +9,7 @@ export const Signup = () => {
 
     const [name, setName] = useState('');
     const [username, setUsername] = useState('');
+    // const usernameRef=useRef('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [newPassword, setNewPassword] = useState('');
@@ -17,11 +18,12 @@ export const Signup = () => {
     const [showNewPassword, setShowNewPassword] = useState(false);
     const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false);
 
-    const usernameAndPasswordMatchingError = (((newPassword && username) && (username === newPassword)) ? true : false);
     const confirmPasswordError = (((newPassword && confirmNewPassword && newPassword !== confirmNewPassword ? true : false)))
     const [userNameError, setUserNameError] = useState(false);
     const [emailError, setEmailError] = useState(false);
     const [phoneNumberError, setPhoneNumberError] = useState(false);
+    const [newPasswordError, setNewPasswordError] = useState(false);
+    const [helpingTextNewPassword, setHelpingTextNewPassword] = useState('');
 
     const confirmNewPassError = (((newPassword && confirmNewPassword) && (newPassword !== confirmNewPassword)) ? true : false);
 
@@ -35,66 +37,108 @@ export const Signup = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('submit clicked')
         console.log('name',name)
         console.log('username',username)
         console.log('email',email)
         console.log('phoneNumber',phoneNumber)
+        // last character of newPassword String is not comming inside handleSubmit but comming in outside of this function
         console.log('newPassword',newPassword)
         console.log('confirmNewPassword',confirmNewPassword)
 
         if (!(name && username && email && phoneNumber && newPassword && confirmNewPassword)) return alert('Fill all the details')
 
+        let isErrorOccured = false;
+
         const checkUsername = containsAlphabetsNumbersAndSpecialChars(username);
         const checkEmail = email.includes('@' && 'com') ? true : false;
-        const checkPhoneNumber = phoneNumber.length>11 ? true : false;
-        if (!checkUsername) return setUserNameError(prev => !prev);
-        if (!checkEmail) return setEmailError(prev => !prev);
-        if (!checkPhoneNumber) return setPhoneNumberError(prev => !prev);
+        const checkPhoneNumber = phoneNumber.length > 11 ? true : false;
+        const checkNewPassword = containsAlphabetsNumbersAndSpecialChars(newPassword);
+        if (!checkUsername) {
+            setUserNameError(prev => !prev);
+            isErrorOccured = true;
+        }
+        if (!checkEmail) {
+            setEmailError(prev => !prev);
+            isErrorOccured = true;
+        }
+        if (!checkPhoneNumber) {
+            setPhoneNumberError(prev => !prev);
+            setHelpingTextNewPassword('')
+            isErrorOccured = true;
+        }
+        if (!checkNewPassword || username === newPassword) {
+            setNewPasswordError(prev => !prev);
+            isErrorOccured = true;
+            // console.log('username', username, 'type', typeof username);
+            // console.log('newPassword', newPassword, 'type', typeof newPassword);
+            // console.log('!checkNewPassword',!checkNewPassword)
+            // console.log('username===newPassword ', username === newPassword)
+            // console.log('checkNewPassword', checkNewPassword)
+            // console.log('username===newPassword && !checkNewPassword', username === newPassword && !checkNewPassword)
+            if (username === newPassword && !checkNewPassword) {
+                console.log('hi in first')
+                console.log('username===newPassword', username === newPassword)
+                console.log('checkNewPassword', checkNewPassword)
+                setHelpingTextNewPassword(`USERNAME and PASSWORD should not be same, USERNAME must contain combination of alphanumeric values with special characters only`)
+            } else if (username === newPassword) {
+                setHelpingTextNewPassword(`USERNAME and PASSWORD should not be same`);
+            } else setHelpingTextNewPassword(`USERNAME must contain combination of alphanumeric values with special characters only`);
+        }
+
+        if (isErrorOccured) return;
 
         if (username && newPassword) return alert('Your details has been verified, Thanks!');
         else return alert('Fill all the details');
     }
 
+    console.log('username outside', username)
+
     return (
         <div>
             <Navbar headtext={'Create new Account'} />
-            <Box
-                component="form"
-                sx={{ '& .MuiTextField-root': { m: 1 } }}
-                noValidate
-                autoComplete="off"
-                className='mt-[7vh] w-[70vw] mx-auto relative'
-                onSubmit={handleSubmit}
-            >
-                <div className='mb-[2vh] flex justify-between flex-col lg:flex-row'>
+            <form autoComplete='off' className='mt-[7vh] w-[70vw] mx-auto' onSubmit={handleSubmit}>
+                <div className='mb-[8vh] flex justify-between flex-col lg:flex-row gap-10'>
                     <TextField id="filled-basic" label="NAME" onKeyDown={(event) => {
-                        console.log('ALPHA_REGEX.test(event.key)',ALPHA_REGEX.test(event.key))
                         if (!ALPHA_REGEX.test(event.key)) event.preventDefault();
                         setName(event.target.value);
-                    }} variant="filled" className='w-full lg:w-1/2' />
+                    }} variant="standard" className='w-full lg:w-1/2'
+                        InputLabelProps={{ sx: { paddingLeft: '15px', '&.MuiInputLabel-shrink': { fontSize: '14px' } } }}
+                        inputProps={{ sx: { paddingLeft: '15px' } }}
+                    />
                     <TextField id="2" label="USERNAME" error={userNameError} helperText={userNameError ? 'USERNAME must contain combination of alphanumeric values with special characters only' : ''} onKeyDown={(event) => {
                         if (!ALPHA_NUM_CHAR_REGEX.test(event.key)) event.preventDefault();
                         setUsername(event.target.value);
-                    }} variant="standard" className='w-full lg:w-1/2' />
+                    }} variant="standard" className='w-full lg:w-1/2' 
+                    InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
+                    inputProps={{sx:{paddingLeft: '15px'}}}
+                    />
                 </div>
-                <div className='mb-[6vh] flex justify-between flex-col lg:flex-row'>
-                    <TextField id="email" type='email' label="EMAIL" error={emailError} helperText={emailError ? 'Your Email is not invalid' : ''} onChange={e => setEmail(e.target.value)} variant="standard" className='w-full lg:w-1/2' />
-                    <TextField id="phone" type='number' label="PHONE NO." defaultValue="91" onChange={e => setPhoneNumber(e.target.value)} variant="standard" className='w-full lg:w-1/2' />
+                <div className='mb-[8vh] flex justify-between flex-col lg:flex-row gap-10'>
+                    <TextField id="email" type='email' label="EMAIL"
+                      InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
+                      inputProps={{sx:{paddingLeft: '15px'}}}
+                    error={emailError} helperText={emailError ? 'Your EMAIL is invalid' : ''} onChange={e => setEmail(e.target.value)} variant="standard" className='w-full lg:w-1/2' />
+                    <TextField id="phone" type='number' label="PHONE NO."
+                      InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
+                      inputProps={{sx:{paddingLeft: '15px'}}}
+                     error={phoneNumberError} helperText={phoneNumberError ? 'Your  PHONE NO. is invalid, it must greater than 9 numbers' : ''} defaultValue="91" onChange={e => setPhoneNumber(e.target.value)} variant="standard" className='w-full lg:w-1/2' />
                 </div>
-                <div className='mb-[6vh] flex justify-between flex-col lg:flex-row'>
-                    <TextField id="newPassword" error={usernameAndPasswordMatchingError} helperText={usernameAndPasswordMatchingError ? 'USERNAME and PASSWORD should not be same' : ''} variant="standard" className='w-full lg:w-1/2'
+                <div className='mb-[8vh] flex justify-between flex-col lg:flex-row gap-10'>
+                    <TextField id="newPassword" error={helpingTextNewPassword ? true : false} helperText={helpingTextNewPassword} variant="standard" className='w-full lg:w-1/2'
                         type={showNewPassword ? 'text' : 'password'}
                         label="NEW PASSWORD" onKeyDown={(event) => {
-                            if (!ALPHA_REGEX.test(event.key)) event.preventDefault()
+                            if (!ALPHA_NUM_CHAR_REGEX.test(event.key)) event.preventDefault()
                             setNewPassword(event.target.value);
                         }}
+                        InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
                         InputProps={{
+                            sx: {paddingLeft: '15px'},
                             endAdornment:
                                 <InputAdornment position="end">
                                     <IconButton
                                         aria-label="toggle password visibility"
                                         onClick={handleClickShowNewPassword}
-                                        onMouseDown={(e) => e.preventDefault()}
                                     >
                                         {showNewPassword ? <VisibilityOff /> : <Visibility />}
                                     </IconButton>
@@ -102,18 +146,19 @@ export const Signup = () => {
                         }}
                     />
                     <TextField id="confirmNewPassword" error={confirmPasswordError} helperText={confirmNewPassError ? 'CONFIRM PASSWORD must be same as PASSWORD' : ''} variant="standard" className='w-full lg:w-1/2'
-                        type={showNewPassword ? 'text' : 'password'}
+                        type={showConfirmNewPassword ? 'text' : 'password'}
                         label="CONFIRM NEW PASSWORD" onKeyDown={(event) => {
                             if (!ALPHA_NUM_CHAR_REGEX.test(event.key)) event.preventDefault()
                             setConfirmNewPassword(event.target.value);
                         }}
+                        InputLabelProps={{sx: {paddingLeft: '15px','&.MuiInputLabel-shrink':{fontSize:'14px'}}}}
                         InputProps={{
+                            sx: {paddingLeft: '15px'},
                             endAdornment:
                                 <InputAdornment position="end">
                                     <IconButton
                                         aria-label="toggle password visibility"
-                                        onClick={() => setShowConfirmNewPassword((prev) => !prev)}
-                                        onMouseDown={handleClickShowConfirmNewPassword}
+                                        onClick={handleClickShowConfirmNewPassword}
                                     >
                                         {showConfirmNewPassword ? <VisibilityOff /> : <Visibility />}
                                     </IconButton>
@@ -121,8 +166,8 @@ export const Signup = () => {
                         }}
                     />
                 </div>
-                <button className='bg-[#0a856d] px-20 py-3 rounded-xl mt-10 absolute right-5'>SIGN UP</button>
-            </Box>
+                <button className='bg-[#0a856d] text-white px-20 py-2 rounded-xl mt-10 float-right'>SIGN UP</button>
+            </form>
         </div>
     )
 }
