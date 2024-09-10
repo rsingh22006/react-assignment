c96d143f4d6c5bc2f521debc5f82434cb1cd6930
Ritik <rsingh2206@daxsws.com>
2024-09-10 14:48:29 +0530

    deleted unused useFocusAndBlur customHook

--- src/customHooks/useFocusAndBlur.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const useFocusAndBlur = (initData) => {
-    const [focusData, setFocusData] = React.useState(initData);
-    const handleChangeFocusAndBlur = (event, type) => {
-        setFocusData({ ...focusData, [event.target.name]: type === 'focus' ? true : false })
-      }
-  return {focusData,handleChangeFocusAndBlur}
-}
