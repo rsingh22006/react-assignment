03f1b5ddea3c2f96269d0c93f656a40c6f26df77
Ritik <rsingh2206@daxsws.com>
2024-09-10 15:13:08 +0530

    removed unused parameters from InputField component & modified css of InputField label

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,7 +1,7 @@
 import { handleKeyDown, handlePaste } from '../utils/handleFuncLogic';
 import { VisibilityIcon } from './VisibilityIcon';
 
-export const InputField = ({ name, type = 'text', value, labelValue, focus, show, error, handleChange, handleChangeFocusAndBlur, handleClickShow }) => {
+export const InputField = ({ name, type = 'text', value, labelValue, show, error, handleChange, handleClickShow }) => {
     const isPhoneNum = name === 'phoneNumber';
     return (
         <div className='relative flex flex-col w-full justify-center align-center gap-y-2 m-auto'>
@@ -22,8 +22,10 @@ export const InputField = ({ name, type = 'text', value, labelValue, focus, show
                 </button>
             }
             {isPhoneNum && <span className='absolute top-[20%] left-[-15px] hidden peer-focus:block'>+91</span>}
-            <label htmlFor={name} className={`absolute text-gray-500 ${error ? 'text-error-color' : 'text-[#0a856d]'} duration-300 transform -translate-y-4 scale-75 top-4 origin-[0] left-[3.5%] peer-focus:text-sm peer-focus:text-[#0a856d] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}>
-                {labelValue}
+            <label
+                htmlFor={name}
+                className={`absolute text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 origin-[0] left-[3.5%] peer-focus:text-sm ${error ? 'peer-focus:text-error-color text-error-color' : 'peer-focus:text-[#0a856d]'} peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}
+            >{labelValue}
             </label>
             <p className={`text-red text-left text-error-color text-xs sm:text-sm pl-4 ${error ? 'visible' : 'invisible'} h-10`}>{error}</p>
         </div>
