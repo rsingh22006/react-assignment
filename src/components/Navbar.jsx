42715bfb1fcf2cc35dadc5162a6572e6b3849b46
Ritik <rsingh2206@daxsws.com>
2024-09-20 17:41:35 +0530

    fixed indentation in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { handleNavDetails } from "../utils/handleFuncLogic";
 import { useLocation } from "react-router-dom";
 
 export const Navbar = () => {
-    const {isAuthenticated} = useContext(UserContext);
+    const { isAuthenticated } = useContext(UserContext);
     const pathArr = useLocation().pathname.split('/');
     const path = pathArr.find(el => el !== '');
-    const {text,paraText,headtextSize} = handleNavDetails(path,isAuthenticated);
+    const { text, paraText, headtextSize } = handleNavDetails(path, isAuthenticated);
     return (
         <div className='navDiv'>
             <p className={`navFirstP ${headtextSize ? `text-${headtextSize}` : ''}`}>
